bcf1083f705c30e766e4d2134ce27700b18e6086
Raajn Patel <rndpatel@gmail.com>
2019-08-22 15:42:00 -0700

    Form starting to output data.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import { Form } from 'formik';
 import './App.css';
+import FormikLoginForm from "./Form.js";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+        <h1>Onboarding</h1>
+      <FormikLoginForm />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -3,34 +3,43 @@ import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-const LoginForm = ({errors, touched, values})=> {
+const LoginForm = ({errors, touched, values, status})=> {
     const [user, setUser] = useState([]);
 
     useEffect(() => {
-        if (status) {
+        if(status) {
             setUser([...user, status]);
         }
-    }, [])
+    }, [status]);
 
     return (
         <div>
             <Form className = "form-component">
                 {touched.name && errors.name}
-                <Field component = "input" type = "text" name = "name" placeholder = "name"/>
-                {touched.name && errors.name}
-                <Field component = "input" type = "text" name = "name" placeholder = "name"/>
-                {touched.name && errors.name}
-                <Field component = "input" type = "text" name = "name" placeholder = "name"/>
+                <Field component = "input" type = "text" name = "name" placeholder = "Name"/>
+                <br/><br/>
+                {touched.password && errors.password}
+                <Field component = "input" type = "text" name = "password" placeholder = "Password"/>
+                <br/><br/>
+                {touched.email && errors.email}
+                <Field component = "input" type = "text" name = "email" placeholder = "Valid Email"/>
+                <br/><br/>
+                {touched.tos && errors.tos}
                 <label>
                     <Field type="checkbox" name="tos" checked={values.tos} />
                     Accept TOS
                 </label>
+                <br/>
                 <label>
-                    <Field type="checkbox" name="spam" checked={values.tos} />
-                    Accept TOS
+                    <Field type="checkbox" name="spam" checked={values.spam} />
+                    Mailing List
                 </label>
+            <br/>
                 <button>Submit</button>
             </Form>
+            {user.map(person => {
+                return <p key={person.email}>{person.name}</p>;
+            })}
         </div>
     )
 };
@@ -53,17 +62,25 @@ const FormikLoginForm = withFormik({
         tos: Yup.boolean().oneOf([true], "Please Accept Our Terms and Conditions")
     }),
 
-    handleSubmit(values, {resetForm, setErrors, setSubmitting, setStatus}) {
-        axios
-            .post("https://reqres.in/api/users", values)
-            .then (res => {
-                setSubmitting(false);
-                let user = res.data.name;
-                let email = res.data.email;
-                setStatus({name: user, email: email});
-                resetForm();
-            })
+    handleSubmit(values, { resetForm, setErrors, setSubmitting, setStatus }) {
+        if (values.email === "waffle@syrup.com") {
+            setErrors({ email: "That email is already taken" });
+        } else {
+            axios
+                .post("https://reqres.in/api/users", values)
+                .then(res => {
+
+                    setSubmitting(false);
+                    let user = res.data.name;
+                    let email = res.data.email;
+                    setStatus({ name: user, email: email });
+                    resetForm();
+                })
+                .catch(err => {
+                    console.log(err);
+                    setSubmitting(false);
+                });
+        }
     }
 })(LoginForm);
-
-export default FormikLoginForm
+export default FormikLoginForm;
